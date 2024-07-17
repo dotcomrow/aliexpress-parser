4017f07361f28a47e468b604e454b48b5ec73519
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-07-17 07:19:48 -0400

    cleaning up repo
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- dist/Parser.d.ts
+++
@@ -1,6 +0,0 @@
-export default class AliExpressParser {
-    private baseURL;
-    constructor();
-    search(query: string, page?: number): Promise<any[]>;
-    private parseHTML;
-}
--- test/index.test.ts
+++ test/index.test.ts
@@ -3,5 +3,5 @@ import Parser from '../src/Parser.js';
 var parser = new Parser();
 parser.search('iphone').then((item) => {
     
-    console.log();
+    console.log(item);
 });
